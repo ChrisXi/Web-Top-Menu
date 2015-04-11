860aa889fadceee9249a5af98ac1e3fd4986c2cb
Xi Yang <clcblueljw@gmail>
2015-04-11 17:17:50 -0400

    add box shadow activated by scroll

--- jquery-topmenu/topmenu.js
+++ jquery-topmenu/topmenu.js
@@ -2,5 +2,15 @@ $(document).ready(function() {
 	$('#topmenu_parentitem').hover(function() { 
 		$(this).find('ul>li').stop().fadeToggle(10);
 	}); 
-}); 
+});
 
+$(document).ready(function() { 
+	var altura = $('#topmenu_menu').offset().top;
+	$(window).on('scroll', function() { 
+		if ($(window).scrollTop() > altura+5) {
+			$('#topmenu_menu').addClass('topmenu_menufloat');
+		} else {
+			$('#topmenu_menu').removeClass('topmenu_menufloat');
+		}
+	}); 
+}); 
